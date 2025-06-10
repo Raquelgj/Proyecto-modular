a335854b37e264418d8fe0e62d438a8540facbb2
Raquel <raquel.gonzalez@hagamoshogar.com>
2025-06-10 18:58:09 +0200

    modificando editar cuenta

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -5,7 +5,7 @@
 @section('content')
 <div class="py-12">
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-        
+
         <!-- Formulario de actualización de información del perfil -->
         <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
             <div class="max-w-xl">
@@ -20,12 +20,6 @@
             </div>
         </div>
 
-        <!-- Formulario de eliminación del usuario -->
-        <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-            <div class="max-w-xl">
-                @include('profile.partials.delete-user-form')
-            </div>
-        </div>
     </div>
 </div>
 @endsection
